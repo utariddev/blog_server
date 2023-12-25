e93845ac1d191826992180a6857e08a56c2ac95b
utarid <utarid@protonmail.com>
2023-12-25 14:59:02 +0100

    create spring app

---
+++ src/main/java/org/utarid/server/UtaridServerApplication.java
@@ -0,0 +1,13 @@
+package org.utarid.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtaridServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtaridServerApplication.class, args);
+    }
+
+}
